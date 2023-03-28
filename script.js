1dc7efec9cd5e55fba90fffe68374763acc1dfae
imagine <91915381+ImaginePog@users.noreply.github.com>
2023-03-28 10:21:53 +0545

    Add range selection for grid size

--- script.js
+++ script.js
@@ -1,21 +1,38 @@
 const canvas = document.querySelector(".canvas");
-let gridSize = 1;
+const gridSizeSlider = document.querySelector("#size-slider");
+const displaySize = document.querySelector(".size-display");
 
 function paintSquare(e) {
 	e.target.style.backgroundColor = "Red";
 }
 
-for (let i = 0; i < gridSize; ++i) {
-	const row = document.createElement("div");
-	row.classList.add("row");
-
-	for (let j = 0; j < gridSize; ++j) {
-		const square = document.createElement("div");
-		square.classList.add("square");
-		square.addEventListener("mouseenter", paintSquare);
-
-		row.appendChild(square);
-	}
-
-	canvas.appendChild(row);
+function resetCanvas() {
+	canvas.replaceChildren();
 }
+
+function updateGridSize() {
+	gridSize = gridSizeSlider.value;
+	displaySize.textContent = `${gridSize}x${gridSize}`;
+	resetCanvas();
+	createCanvas();
+}
+
+function createCanvas() {
+	for (let i = 0; i < gridSize; ++i) {
+		const row = document.createElement("div");
+		row.classList.add("row");
+
+		for (let j = 0; j < gridSize; ++j) {
+			const square = document.createElement("div");
+			square.classList.add("square");
+			square.addEventListener("mouseenter", paintSquare);
+
+			row.appendChild(square);
+		}
+
+		canvas.appendChild(row);
+	}
+}
+
+gridSizeSlider.addEventListener("input", updateGridSize);
+updateGridSize();
