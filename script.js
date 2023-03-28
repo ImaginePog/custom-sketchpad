39881afeb3121183623fa3543b0e545608c9e8ef
imagine <91915381+ImaginePog@users.noreply.github.com>
2023-03-28 09:37:22 +0545

    Add paint on hover

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
 const canvas = document.querySelector(".canvas");
 
+function paint(e) {
+	e.target.style.backgroundColor = "Red";
+}
+
 for (let i = 0; i < 50; ++i) {
 	const row = document.createElement("div");
 	row.classList.add("row");
 
 	for (let j = 0; j < 50; ++j) {
-		const column = document.createElement("div");
-		column.classList.add("square");
+		const square = document.createElement("div");
+		square.classList.add("square");
+		square.addEventListener("mouseenter", paintSquare);
 
-		row.appendChild(column);
+		row.appendChild(square);
 	}
 
 	canvas.appendChild(row);
